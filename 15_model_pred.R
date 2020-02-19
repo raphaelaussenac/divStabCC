553adcb874e44cd8352da12c4de69de05fdadfcd
Raphaël Aussenac <raphael.aussenac@gmail.com>
2020-02-19 14:44:47 +0100

    optimise prediction code and correct doParallel warning (already exporting variables)

--- 15_model_pred.R
+++ 15_model_pred.R
@@ -1,5 +1,6 @@
 # Delete all objects in the work space
 rm(list=setdiff(ls(), c("tex", "dra")))
+start_time <- Sys.time()
 
 ####################################################
 ## Packages
@@ -39,21 +40,11 @@ new <- function(data = data){
 }
 
 ####################################################
-##
+## prediction function
 ####################################################
-setwd("./data/futurClimate/allClimateData")
-fileNames <- Sys.glob("*.rdata")
-fileNames
 
-# Choose the work directory = folder
-if (Sys.info()["sysname"] == "Darwin"){
-  mainBasePath <- "/Users/raphaelaussenac/Documents/GitHub/divStabCC"
-  setwd(mainBasePath)
-} else if (Sys.info()["sysname"] == "Windows"){
-  mainBasePath <- "C:/Users/raphael.aussenac/Documents/GitHub/divStabCC"
-  setwd(mainBasePath)
-}
-
+fileNames <- Sys.glob(file.path("./data/futurClimate/allClimateData", "*.rdata"))
+fileNames <- fileNames
 
 pred_mod <- function(s = c("SAB", "PET")){
 
@@ -69,10 +60,11 @@ pred_mod <- function(s = c("SAB", "PET")){
   predictions <- as.data.frame(matrix(ncol = 9 + nsim))
   colnames(predictions) <- c("ID_PET_MES", "ID_ARB", "ESSENCE", "prop_SAB_BA","prop_PET_BA", "yr", paste("V", seq(1,nsim,1), sep=""), "rcp", "mod", "rcpmod" )
   for (i in fileNames){
-    load(paste(getwd(), "/data/futurClimate/allClimateData/",i, sep = ""))
+    load(i)
     data <- new(data)
     sp <- s
     newdata <- prepare_mod_data_futur(data = data, sp = sp)
+    rm(data) # free up memory
 
     # ############### model_select
     # fix <- model.matrix(~ BAtot_CM2HA + sizeE + mixE + compethard + competsoft + DC + DCp + Tannual + Pannual + texture + drainage + texture:drainage + mixE:BAtot_CM2HA + sizeE:DC + sizeE:DCp  + compethard:DC + competsoft:DC + compethard:DCp + competsoft:DCp + mixE:DC + mixE:DCp + texture:sizeE + texture:mixE + texture:compethard + texture:competsoft + texture:DC + texture:DCp + texture:Tannual + texture:Pannual + drainage:sizeE + drainage:mixE + drainage:compethard + drainage:competsoft + drainage:DC + drainage:DCp + drainage:Tannual + drainage:Pannual, data = newdata)
@@ -84,56 +76,33 @@ pred_mod <- function(s = c("SAB", "PET")){
     pred <- predictInterval(mod, newdata = newdata, which = "fixed", level = 0.95, n.sims = nsim, stat = "median", include.resid.var = TRUE, returnSims = TRUE)
     pred <- as.data.frame(attr(pred, "sim.results"))
     newdata <- cbind(newdata, pred)
+    rm(pred) # free up memory
     # exp(log(BAI+1))
     newdata[, paste("V", seq(1,nsim,1), sep="")] <- exp(newdata[, paste("V", seq(1,nsim,1), sep="")]) - exp(log(1))
     newdata$rcp <- substr(i, 1, 5)
     newdata$mod <- substr(i, 7, 11)
     newdata$rcpmod <- substr(i, 1, 11)
     model <- newdata[, colnames(predictions)]
+    rm(newdata) # free up memory
     predictions <- rbind(predictions, model)
+    rm(model) # free up memory
   }
   predictions <- predictions[-1, ]
 
-  # save
-  # setwd("~/owncloud/Work_directory/Analysis/chapitre_3/03_mixed_model/output")
-  # Choose the work directory = folder
-  if (Sys.info()["sysname"] == "Darwin"){
-    mainBasePath <- "/Users/raphaelaussenac/Documents/GitHub/divStabCC/modelOutput"
-    setwd(mainBasePath)
-  } else if (Sys.info()["sysname"] == "Windows"){
-    mainBasePath <- "C:/Users/raphael.aussenac/Documents/GitHub/divStabCC/modelOutput"
-    setwd(mainBasePath)
-  }
-
-  save(predictions, file = paste("QC_BAI_", s, "_T0D0", ".rdata", sep = "")) # change name according to T and D
-
-  # setwd("./data/futurClimate/allClimateData")
-  # Choose the work directory = folder
-  if (Sys.info()["sysname"] == "Darwin"){
-    mainBasePath <- "/Users/raphaelaussenac/Documents/GitHub/divStabCC"
-    setwd(mainBasePath)
-  } else if (Sys.info()["sysname"] == "Windows"){
-    mainBasePath <- "C:/Users/raphael.aussenac/Documents/GitHub/divStabCC"
-    setwd(mainBasePath)
-  }
+  save(predictions, file = paste(getwd(), "/modelOutput/", "QC_BAI_", s, "_T0D0", ".rdata", sep = "")) # change name according to T and D
 
 }
 
-# We replicated the addresses nCores times.
-cl <- makePSOCKcluster(2)
+####################################################
+## Run predictions on 2 cores
+####################################################
+
+cl <- makeCluster(2)
 registerDoParallel(cl)
 spe <- c("SAB", "PET")
-foreach(i = 1:length(spe), .packages = "merTools", .export = c("new", "prepare_mod_data_futur", "fileNames")) %dopar% {
+foreach(i = 1:length(spe), .packages = "merTools") %dopar% {
   pred_mod(s = spe[i])
 }
 
-# unregister cluster
-stopCluster(cl)
-
-# registerDoSEQ()
-#
-#
-# unregister <- function() {
-#   env <- foreach:::.foreachGlobals
-#   rm(list=ls(name=env), pos=env)
-# }
+end_time <- Sys.time()
+end_time - start_time
