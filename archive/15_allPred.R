82c8baaad8fa173f4d15177ef7ef95320da7cefb
Raphaël Aussenac <raphael.aussenac@gmail.com>
2020-04-17 22:30:02 +0200

    archive2

---
+++ archive/15_allPred.R
@@ -0,0 +1,111 @@
+# Delete all objects in the work space
+rm(list = ls(all = TRUE))
+gc()
+
+# Choose the work directory = folder
+if (Sys.info()["sysname"] == "Darwin"){
+  mainBasePath <- "/Users/raphaelaussenac/Documents/GitHub/divStabCC"
+  setwd(mainBasePath)
+} else if (Sys.info()["sysname"] == "Windows"){
+  mainBasePath <- "C:/Users/raphael.aussenac/Documents/GitHub/divStabCC"
+  setwd(mainBasePath)
+}
+
+tex <- 3
+dra <- 1
+source('./15_model_pred.R')
+
+########################################################################################################
+
+# Delete all objects in the work space
+rm(list = ls(all = TRUE))
+gc()
+
+# Choose the work directory = folder
+if (Sys.info()["sysname"] == "Darwin"){
+  mainBasePath <- "/Users/raphaelaussenac/Documents/GitHub/divStabCC"
+  setwd(mainBasePath)
+} else if (Sys.info()["sysname"] == "Windows"){
+  mainBasePath <- "C:/Users/raphael.aussenac/Documents/GitHub/divStabCC"
+  setwd(mainBasePath)
+}
+
+tex <- 3
+dra <- 2
+source('./15_model_pred.R')
+
+########################################################################################################
+# Delete all objects in the work space
+rm(list = ls(all = TRUE))
+gc()
+
+# Choose the work directory = folder
+if (Sys.info()["sysname"] == "Darwin"){
+  mainBasePath <- "/Users/raphaelaussenac/Documents/GitHub/divStabCC"
+  setwd(mainBasePath)
+} else if (Sys.info()["sysname"] == "Windows"){
+  mainBasePath <- "C:/Users/raphael.aussenac/Documents/GitHub/divStabCC"
+  setwd(mainBasePath)
+}
+
+tex <- 2
+dra <- 1
+source('./15_model_pred.R')
+
+########################################################################################################
+
+# Delete all objects in the work space
+rm(list = ls(all = TRUE))
+gc()
+
+# Choose the work directory = folder
+if (Sys.info()["sysname"] == "Darwin"){
+  mainBasePath <- "/Users/raphaelaussenac/Documents/GitHub/divStabCC"
+  setwd(mainBasePath)
+} else if (Sys.info()["sysname"] == "Windows"){
+  mainBasePath <- "C:/Users/raphael.aussenac/Documents/GitHub/divStabCC"
+  setwd(mainBasePath)
+}
+
+tex <- 2
+dra <- 2
+source('./15_model_pred.R')
+
+########################################################################################################
+# Delete all objects in the work space
+rm(list = ls(all = TRUE))
+gc()
+
+# Choose the work directory = folder
+if (Sys.info()["sysname"] == "Darwin"){
+  mainBasePath <- "/Users/raphaelaussenac/Documents/GitHub/divStabCC"
+  setwd(mainBasePath)
+} else if (Sys.info()["sysname"] == "Windows"){
+  mainBasePath <- "C:/Users/raphael.aussenac/Documents/GitHub/divStabCC"
+  setwd(mainBasePath)
+}
+
+tex <- 1
+dra <- 1
+source('./15_model_pred.R')
+
+########################################################################################################
+
+# Delete all objects in the work space
+rm(list = ls(all = TRUE))
+gc()
+
+# Choose the work directory = folder
+if (Sys.info()["sysname"] == "Darwin"){
+  mainBasePath <- "/Users/raphaelaussenac/Documents/GitHub/divStabCC"
+  setwd(mainBasePath)
+} else if (Sys.info()["sysname"] == "Windows"){
+  mainBasePath <- "C:/Users/raphael.aussenac/Documents/GitHub/divStabCC"
+  setwd(mainBasePath)
+}
+
+tex <- 1
+dra <- 2
+source('./15_model_pred.R')
+
+########################################################################################################
