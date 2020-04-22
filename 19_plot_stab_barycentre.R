ace2158968295218419ca9511a810bc29809b157
Raphaël Aussenac <raphael.aussenac@gmail.com>
2020-04-22 14:06:10 +0200

    calculate median predictions

--- 19_plot_stab_barycentre.R
+++ 19_plot_stab_barycentre.R
@@ -86,9 +86,12 @@ diffTS$diffTS <- diffTS[, '2081-2100'] - diffTS[, '1986-2005']
 ## calculate envelopes
 ####################################################
 
+# TODO:
+# - remplacer enveloppes internes par médiane
+# - diagramme soustraction entre courbe overyielding et diagonale (prod attendues des espèces en mélange)
+
 # choose colors for rcp
 cbPalette <- c("cadetblue3", "gold2")
-
 
 # plot Mean
 # min, max and CI for each RCP
@@ -96,13 +99,16 @@ df_min <- ddply(standDf, .(rcp, propSAB, period), summarise, min = min(standMean
 df_max <- ddply(standDf, .(rcp, propSAB, period), summarise, max = max(standMeanGrowth))
 df_CImin <- ddply(standDf, .(rcp, propSAB, period), summarise, CImin = quantile(standMeanGrowth,0.375))
 df_CImax <- ddply(standDf, .(rcp, propSAB, period), summarise, CImax = quantile(standMeanGrowth,0.625))
-dfMean <- cbind(df_min, 'max' = df_max$max, 'CImin' = df_CImin$CImin, 'CImax' = df_CImax$CImax)
+dfMed <- ddply(standDf, .(rcp, propSAB, period), summarise, med = median(standMeanGrowth))
+dfMean <- cbind(df_min, 'max' = df_max$max, 'CImin' = df_CImin$CImin, 'CImax' = df_CImax$CImax, 'med' = dfMed$med)
 
 ggplot(data = dfMean) +
-geom_ribbon(aes(x = propSAB, ymax = max, ymin = min, fill = rcp), alpha = 0.1) +
-geom_ribbon(aes(x = propSAB, ymax = CImax, ymin = CImin, fill = rcp), alpha = 0.6) +
+geom_ribbon(aes(x = propSAB, ymax = max, ymin = min, fill = rcp), alpha = 0.2) +
+# geom_ribbon(aes(x = propSAB, ymax = CImax, ymin = CImin, fill = rcp), alpha = 0.6) +
+geom_line(aes(x = propSAB, y = med, col = rcp), size = 1) +
+scale_color_manual(values=cbPalette) +
 scale_fill_manual(values=cbPalette) +
-geom_smooth(data = subset(dfMean, period == "1986-2005"), aes(x = propSAB, y = CImax), method = lm, col = "black", linetype = "dashed", size = 0.5) +
+# geom_smooth(data = subset(dfMean, period == "1986-2005"), aes(x = propSAB, y = CImax), method = lm, col = "black", linetype = "dashed", size = 0.5) +
 facet_grid( ~ period) +
 scale_x_continuous(breaks=seq(0, 1, by = 0.1), expand = c(0, 0)) +
 xlab('proportion of fir')+
@@ -125,11 +131,13 @@ df_min <- ddply(standDf, .(rcp, propSAB, period), summarise, min = min(standVar)
 df_max <- ddply(standDf, .(rcp, propSAB, period), summarise, max = max(standVar))
 df_CImin <- ddply(standDf, .(rcp, propSAB, period), summarise, CImin = quantile(standVar,0.375))
 df_CImax <- ddply(standDf, .(rcp, propSAB, period), summarise, CImax = quantile(standVar,0.625))
-dfVar <- cbind(df_min, 'max' = df_max$max, 'CImin' = df_CImin$CImin, 'CImax' = df_CImax$CImax)
+dfMed <- ddply(standDf, .(rcp, propSAB, period), summarise, med = median(standVar))
+dfVar <- cbind(df_min, 'max' = df_max$max, 'CImin' = df_CImin$CImin, 'CImax' = df_CImax$CImax, 'med' = dfMed$med)
 
 ggplot(data = dfVar) +
 geom_ribbon(aes(x = propSAB, ymax = max, ymin = min, fill = rcp), alpha = 0.1) +
-geom_ribbon(aes(x = propSAB, ymax = CImax, ymin = CImin, fill = rcp), alpha = 0.6) +
+# geom_ribbon(aes(x = propSAB, ymax = CImax, ymin = CImin, fill = rcp), alpha = 0.6) +
+geom_line(aes(x = propSAB, y = med, col = rcp), size = 1) +
 scale_fill_manual(values=cbPalette) +
 facet_grid( ~ period) +
 scale_x_continuous(breaks=seq(0, 1, by = 0.1), expand = c(0, 0)) +
@@ -151,11 +159,13 @@ df_min <- ddply(standDf, .(rcp, propSAB, period), summarise, min = min(stab))
 df_max <- ddply(standDf, .(rcp, propSAB, period), summarise, max = max(stab))
 df_CImin <- ddply(standDf, .(rcp, propSAB, period), summarise, CImin = quantile(stab,0.375))
 df_CImax <- ddply(standDf, .(rcp, propSAB, period), summarise, CImax = quantile(stab,0.625))
-dfStab <- cbind(df_min, 'max' = df_max$max, 'CImin' = df_CImin$CImin, 'CImax' = df_CImax$CImax)
+dfMed <- ddply(standDf, .(rcp, propSAB, period), summarise, med = median(stab))
+dfStab <- cbind(df_min, 'max' = df_max$max, 'CImin' = df_CImin$CImin, 'CImax' = df_CImax$CImax, 'med' = dfMed$med)
 
 ggplot(data = dfStab) +
 geom_ribbon(aes(x = propSAB, ymax = max, ymin = min, fill = rcp), alpha = 0.1) +
-geom_ribbon(aes(x = propSAB, ymax = CImax, ymin = CImin, fill = rcp), alpha = 0.6) +
+# geom_ribbon(aes(x = propSAB, ymax = CImax, ymin = CImin, fill = rcp), alpha = 0.6) +
+geom_line(aes(x = propSAB, y = med, col = rcp), size = 1) +
 scale_fill_manual(values=cbPalette) +
 facet_grid( ~ period) +
 scale_x_continuous(breaks=seq(0, 1, by = 0.1), expand = c(0, 0)) +
@@ -176,11 +186,13 @@ df_min <- ddply(diffMean, .(rcp, propSAB), summarise, min = min(diffMean))
 df_max <- ddply(diffMean, .(rcp, propSAB), summarise, max = max(diffMean))
 df_CImin <- ddply(diffMean, .(rcp, propSAB), summarise, CImin = quantile(diffMean,0.375))
 df_CImax <- ddply(diffMean, .(rcp, propSAB), summarise, CImax = quantile(diffMean,0.625))
-dfDiffMean <- cbind(df_min, 'max' = df_max$max, 'CImin' = df_CImin$CImin, 'CImax' = df_CImax$CImax)
+dfMed <- ddply(diffMean, .(rcp, propSAB), summarise, med = median(diffMean))
+dfDiffMean <- cbind(df_min, 'max' = df_max$max, 'CImin' = df_CImin$CImin, 'CImax' = df_CImax$CImax, 'med' = dfMed$med)
 
 ggplot(data = dfDiffMean) +
 geom_ribbon(aes(x = propSAB, ymax = max, ymin = min, fill = rcp), alpha = 0.1) +
-geom_ribbon(aes(x = propSAB, ymax = CImax, ymin = CImin, fill = rcp), alpha = 0.6) +
+# geom_ribbon(aes(x = propSAB, ymax = CImax, ymin = CImin, fill = rcp), alpha = 0.6) +
+geom_line(aes(x = propSAB, y = med, col = rcp), size = 1) +
 geom_hline(yintercept = 0, linetype = "dashed", color = "black", size = 0.5) +
 scale_fill_manual(values=cbPalette) +
 scale_x_continuous(breaks=seq(0, 1, by = 0.1), expand = c(0, 0)) +
@@ -201,11 +213,13 @@ df_min <- ddply(diffVar, .(rcp, propSAB), summarise, min = min(diffVar))
 df_max <- ddply(diffVar, .(rcp, propSAB), summarise, max = max(diffVar))
 df_CImin <- ddply(diffVar, .(rcp, propSAB), summarise, CImin = quantile(diffVar,0.375))
 df_CImax <- ddply(diffVar, .(rcp, propSAB), summarise, CImax = quantile(diffVar,0.625))
-dfDiffVar <- cbind(df_min, 'max' = df_max$max, 'CImin' = df_CImin$CImin, 'CImax' = df_CImax$CImax)
+dfMed <- ddply(diffVar, .(rcp, propSAB), summarise, med = median(diffVar))
+dfDiffVar <- cbind(df_min, 'max' = df_max$max, 'CImin' = df_CImin$CImin, 'CImax' = df_CImax$CImax, 'med' = dfMed$med)
 
 ggplot(data = dfDiffVar) +
 geom_ribbon(aes(x = propSAB, ymax = max, ymin = min, fill = rcp), alpha = 0.1) +
-geom_ribbon(aes(x = propSAB, ymax = CImax, ymin = CImin, fill = rcp), alpha = 0.6) +
+# geom_ribbon(aes(x = propSAB, ymax = CImax, ymin = CImin, fill = rcp), alpha = 0.6) +
+geom_line(aes(x = propSAB, y = med, col = rcp), size = 1) +
 scale_fill_manual(values=cbPalette) +
 geom_hline(yintercept = 0, linetype = "dashed", color = "red", size = 2) +
 scale_x_continuous(breaks=seq(0, 1, by = 0.1), expand = c(0, 0)) +
@@ -225,11 +239,13 @@ df_min <- ddply(diffTS, .(rcp, propSAB), summarise, min = min(diffTS))
 df_max <- ddply(diffTS, .(rcp, propSAB), summarise, max = max(diffTS))
 df_CImin <- ddply(diffTS, .(rcp, propSAB), summarise, CImin = quantile(diffTS,0.375))
 df_CImax <- ddply(diffTS, .(rcp, propSAB), summarise, CImax = quantile(diffTS,0.625))
-dfDiffTS <- cbind(df_min, 'max' = df_max$max, 'CImin' = df_CImin$CImin, 'CImax' = df_CImax$CImax)
+dfMed <- ddply(diffTS, .(rcp, propSAB), summarise, med = median(diffTS))
+dfDiffTS <- cbind(df_min, 'max' = df_max$max, 'CImin' = df_CImin$CImin, 'CImax' = df_CImax$CImax, 'med' = dfMed$med)
 
 ggplot(data = dfDiffTS) +
 geom_ribbon(aes(x = propSAB, ymax = max, ymin = min, fill = rcp), alpha = 0.1) +
-geom_ribbon(aes(x = propSAB, ymax = CImax, ymin = CImin, fill = rcp), alpha = 0.6) +
+# geom_ribbon(aes(x = propSAB, ymax = CImax, ymin = CImin, fill = rcp), alpha = 0.6) +
+geom_line(aes(x = propSAB, y = med, col = rcp), size = 1) +
 geom_hline(yintercept = 0, linetype = "dashed", color = "red", size = 2) +
 scale_fill_manual(values=cbPalette) +
 scale_x_continuous(breaks=seq(0, 1, by = 0.1), expand = c(0, 0)) +
